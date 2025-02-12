2f6efe725b647b367b4a01c5a632089326975723
arunkumarpediredla89 <167951925+arunkumarpediredla89@users.noreply.github.com>
2025-02-12 13:52:56 +0530

    AdminQuestionService code added

--- Online-Assessment/Online-Assessment.Web.Client/Program.cs
+++ Online-Assessment/Online-Assessment.Web.Client/Program.cs
@@ -7,4 +7,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 // Add device-specific services used by the Online_Assessment.Shared project
 builder.Services.AddSingleton<IFormFactor, FormFactor>();
 builder.Services.AddSingleton<AdminService>();
+builder.Services.AddSingleton<AdminQuestionService>();
 await builder.Build().RunAsync();
