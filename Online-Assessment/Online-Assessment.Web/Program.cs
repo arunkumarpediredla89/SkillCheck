6d284ad7f9269816b5d1fb1100e5b525ce6d391a
arunkumarpediredla89 <167951925+arunkumarpediredla89@users.noreply.github.com>
2025-02-12 13:52:17 +0530

    AdminQuestionService

--- Online-Assessment/Online-Assessment.Web/Program.cs
+++ Online-Assessment/Online-Assessment.Web/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddRazorComponents()
 // Add device-specific services used by the Online_Assessment.Shared project
 builder.Services.AddSingleton<IFormFactor, FormFactor>();
 builder.Services.AddSingleton<AdminService>();
+builder.Services.AddSingleton<AdminQuestionService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
